6423edb58f91ec53b7d9f392fbb4826ebe75f516
yudesong <yudesong@foxmail.com>
2015-08-05 15:50:20 +0800

    modify

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,8 +1,28 @@
 class WelcomeController < ApplicationController
   def index
-
-
-
-
+  	
   end
+
+  def  login
+  	 @name=params[:name]
+        @passwd=params[:passwd]
+        @user=Manager.find_by_name_and_passwd(@name,@passwd)
+         if @user!=nil
+             redirect_to :action =>"index" ,:controller=>"users"  
+          else   
+             redirect_to :action =>"index"  
+          end
+
+#      @user=User.where(name:"yudesong",email:"yudesong@foxmail.com").order("create_at DESC")
+#      @user=User.where(:name=>"yudesong", :email=>"yudesong@foxmail.com")
+=begin
+
+
+       	 if @name=="yu" && @passwd=="111"
+       	     redirect_to :action =>"index" ,:controller=>"users"
+       	 end
+=end	
+  end
+  	
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
-
+  get 'welcome/login'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
