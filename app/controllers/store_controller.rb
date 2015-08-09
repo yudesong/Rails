6475d8898fc2ea9ca9ac8633c47adb7d23d6c8de
yudesong <yudesong@foxmail.com>
2015-08-09 17:27:29 +0800

    modify

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,8 +4,10 @@ class StoreController < ApplicationController
      #   @products =Product.all
         @products = Product.paginate :page => params[:page], :per_page => 5
         @cart = current_cart
-
-
+        respond_to do |format| 
+        format.html #index.html.erb
+        format.json { render json:Product.all }
+     end 
 
   end
 end
